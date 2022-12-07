b87c6ad122fc5fbc00f3423c2e6b15503ee11c85
sandraG95 <118214786+sandraG95@users.noreply.github.com>
2022-12-07 12:34:19 +0100

    Update holamundo.java
    cambio sintasis

--- proyectoNuevo/src/proyectoNuevo/holamundo.java
+++ proyectoNuevo/src/proyectoNuevo/holamundo.java
@@ -3,9 +3,9 @@ package proyectoNuevo;
 public class holamundo {
 
 	public static void main(String[] args) {
-		public static void imprimeSaludo() { 
-System.out.println(“Hei”);	
-}
+		
+System.out.println(“Hola”);	
+
 
 	}
 
